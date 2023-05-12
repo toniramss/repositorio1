e45176f7d60bc6268c0684fe727a25a879c83c1d
toniramss <127487486+toniramss@users.noreply.github.com>
2023-05-12 11:33:22 +0200

    Add files via upload

---
+++ Booom/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int tiempo = 10;
+        while (tiempo != 0) {
+            System.out.println(tiempo);
+            tiempo = tiempo - 1 ;
+        }
+        System.out.println("Booom");
+    }
+}
